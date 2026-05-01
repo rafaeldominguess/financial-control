ad18fc34aebc0204dbe6c6ef874dccf8680acf6e
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-05-01 20:21:44 -0300

    add Header component and update global styles for button and headings

---
+++ src/app/Components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <header className="bg-gray-800 text-white p-4">
+            <h1 className="text-xl font-bold">Financial Control</h1>
+        </header>
+    )
+};
