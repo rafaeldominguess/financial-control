642d081bed7753ef284ee505cc0d43ab35f6bc60
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-05-02 12:22:41 -0300

    refactor Summary and Dashboard components to adjust margin for improved layout

--- src/app/Components/Dashboard/index.tsx
+++ src/app/Components/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import { Summary } from "../Summary";
 
 export function Dashboard() {
     return (
-        <main className="w-full -mt-16">
+        <main className="w-full">
             <Summary />
         </main>
     )
--- src/app/Components/Summary/index.tsx
+++ src/app/Components/Summary/index.tsx
@@ -1,6 +1,6 @@
 export function Summary() {
     return (
-        <main className="grid grid-cols-3 gap-8 max-w-6xl mx-auto">
+        <main className="grid grid-cols-3 gap-8 max-w-6xl mx-auto -mt-16">
             <div>
                 <header>
                     <p>Entrada</p>
