2d0fdcb168e0d038147c731e3edb0fab8e66f374
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-05-02 11:47:06 -0300

    refactor Header component layout and button styles for improved consistency and responsiveness

--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -2,9 +2,9 @@
 export function Header() {
     return (
         <header className="bg-(--blue) text-white w-full">
-            <div className="max-w-6xl flex justify-between mx-auto px-4 py-20">
+            <div className="max-w-6xl flex justify-between items-center mx-auto px-4 pt-8 pb-30">
                 <h1 className="text-xl font-bold">Financial Control</h1>
-                <button type="button" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Nova Transação</button>
+                <button type="button" className="bg-(--blue-light) text-white text-lg border-0 rounded px-8 h-12 hover:brightness-90 transition-all duration-200">Nova Transação</button>
             </div>
         </header>
     )
