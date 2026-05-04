ba3bd9d88367bde306e8af7777806c3fc2e42253
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-05-04 06:07:29 -0300

    refactor Summary component to enhance layout and styling consistency

--- src/app/Components/Summary/index.tsx
+++ src/app/Components/Summary/index.tsx
@@ -1,25 +1,25 @@
 export function Summary() {
     return (
         <main className="grid grid-cols-3 gap-8 max-w-6xl mx-auto -mt-16">
-            <div>
+            <div className="bg-(--shape) rounded px-8 py-6 text-(--text-title)">
                 <header>
                     <p>Entrada</p>
                 </header>
-                <strong>R$1000,00</strong>
+                <strong className="mt-4 text-3xl font-medium leading-12 block">R$1000,00</strong>
             </div>
 
-            <div>
+            <div className="bg-(--shape) rounded px-8 py-6 text-(--text-title)">
                 <header>
                     <p>Saída</p>
                 </header>
-                <strong>- R$500,00</strong>
+                <strong className="mt-4 text-3xl font-medium leading-12 block">- R$500,00</strong>
             </div>
 
-            <div>
+            <div className="bg-(--green) rounded px-8 py-6 text-(--shape)">
                 <header>
                     <p>Total</p>
                 </header>
-                <strong>R$500,00</strong>
+                <strong className="mt-4 text-3xl font-medium leading-12 block">R$500,00</strong>
             </div>
         </main>
     )
