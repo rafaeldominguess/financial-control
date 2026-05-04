d3a78889322c9053e5514789c4cdc430941545ab
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-05-04 06:26:30 -0300

    refactor Header and Summary components for improved layout and spacing consistency

--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -2,7 +2,7 @@
 export function Header() {
     return (
         <header className="bg-(--blue) text-white w-full">
-            <div className="max-w-6xl flex justify-between items-center mx-auto px-4 pt-8 pb-30">
+            <div className="max-w-6xl flex justify-between items-center mx-auto px-4 pt-8 pb-36">
                 <h1 className="text-xl font-bold">Financial Control</h1>
                 <button type="button" className="bg-(--blue-light) text-white text-lg border-0 rounded px-8 h-12 hover:brightness-90 transition-all duration-200">Nova Transação</button>
             </div>
--- src/app/Components/Summary/index.tsx
+++ src/app/Components/Summary/index.tsx
@@ -1,25 +1,25 @@
 export function Summary() {
     return (
         <main className="grid grid-cols-3 gap-8 max-w-6xl mx-auto -mt-16">
-            <div className="bg-(--shape) rounded px-8 py-6 text-(--text-title)">
+            <div className="bg-(--shape) rounded px-8 py-4 text-(--text-title)">
                 <header>
                     <p>Entrada</p>
                 </header>
-                <strong className="mt-4 text-3xl font-medium leading-12 block">R$1000,00</strong>
+                <strong className="mt-2 text-3xl font-medium leading-12 block">R$1000,00</strong>
             </div>
 
-            <div className="bg-(--shape) rounded px-8 py-6 text-(--text-title)">
+            <div className="bg-(--shape) rounded px-8 py-4 text-(--text-title)">
                 <header>
                     <p>Saída</p>
                 </header>
-                <strong className="mt-4 text-3xl font-medium leading-12 block">- R$500,00</strong>
+                <strong className="mt-2 text-3xl font-medium leading-12 block">- R$500,00</strong>
             </div>
 
-            <div className="bg-(--green) rounded px-8 py-6 text-(--shape)">
+            <div className="bg-(--green) rounded px-8 py-4 text-(--shape)">
                 <header>
                     <p>Total</p>
                 </header>
-                <strong className="mt-4 text-3xl font-medium leading-12 block">R$500,00</strong>
+                <strong className="mt-2 text-3xl font-medium leading-12 block">R$500,00</strong>
             </div>
         </main>
     )
